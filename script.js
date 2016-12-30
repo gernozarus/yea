372ff4e9406f08c17ab021e8f0ac48e291e4bbc1
Gernozarus <zoeckleinzockt@gmail.com>
2016-12-30 23:28:53 +0100

    Scrollbar function javascriptdoc included

---
+++ script.js
@@ -0,0 +1,13 @@
+$(window).scroll( function() {
+    funktion1();
+});
+
+
+function funktion1() {
+
+    var wScroll = $(window).scrollTop();
+    if($(.mentoring).offset().top < wScroll {
+        $(".faces").addClass("launch");
+    }
+
+};
